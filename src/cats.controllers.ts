18b5f4779c4297138de9cb18651f857a7679db45
PrecieuxMudibu <oceangpmb@gmail.com>
2022-11-21 15:02:33 +0100

    feat: Return a text for post requests

--- src/cats.controllers.ts
+++ src/cats.controllers.ts
@@ -1,10 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
   @Get()
   findAll(): string {
     return 'This action returns all cats';
+  }
+
+  @Post()
+  create(): string {
+    return 'This action adds a new cat';
   }
 }
 
